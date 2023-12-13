7126008e15b47c6d52db24842e268511681a26b7
Yashashvi <yashashvi.pandey@sveltetech.com>
2023-12-13 17:30:19 +0530

    changes modals

---
+++ src/modals/rejectModal.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ModalWrapper from '../components/modalWrapper/ModalWrapper'
+
+function RejectModal() {
+  return (
+    <ModalWrapper>RejectModal</ModalWrapper>
+  )
+}
+
+export default RejectModal

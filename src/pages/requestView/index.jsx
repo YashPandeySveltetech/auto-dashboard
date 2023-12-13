2a59540fffb595e13863a5f6d5017157c3004547
vikasveltetech <vikas.mishra@sveltetech.com>
2023-12-13 15:38:06 +0530

    view form page

---
+++ src/pages/requestView/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RequestForm from "../requestPage";
+
+const RequestView = () => {
+  return (
+    <div>
+      <RequestForm />
+    </div>
+  );
+};
+
+export default RequestView;

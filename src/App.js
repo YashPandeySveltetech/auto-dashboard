b4aa03efc0d50c20a5d116135ece2fe5a06874be
Yashashvi <yashashvi.pandey@sveltetech.com>
2023-12-13 17:33:00 +0530

    changes modals

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import RejectModal from "./modals/rejectModal";
 import AppRoute from "./routes/rootRouting";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div>
+      <RejectModal/>
       <BrowserRouter>
         <AppRoute />
       </BrowserRouter>
--- src/components/modalWrapper/ModalWrapper.jsx
+++ src/components/modalWrapper/ModalWrapper.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-// import Card from "../common/card";
+import Card from "../card/index";
 import { useDispatch, useSelector } from "react-redux";
 import { commonCloseModal } from "../../redux/reducers/modalsReducer";
 

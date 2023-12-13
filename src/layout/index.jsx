6cf46e4d6272fe81945e14b63277145cbbb94e78
vikasveltetech <vikas.mishra@sveltetech.com>
2023-12-13 10:54:03 +0530

    home page

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,26 +1,21 @@
-/** @format */
-
-import React from 'react';
-import Sidebar from '../components/sideBar';
-import Header from '../components/header';
+import React from "react";
+import Sidebar from "../components/sideBar";
+import Header from "../components/header";
 
 function Layout({ children }) {
-	return (
-		<div className='flex w-[100%]'>
-			<div className='w-[16%]'>
-				<Sidebar />
-			</div>
-			<div className='w-[84%]'>
-				<div>
-					<Header />
-				</div>
-				<div className='p-10 rounded'>
-					{' '}
-					{children}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex w-[100%]">
+      <div className="w-[16%]">
+        <Sidebar />
+      </div>
+      <div className="w-[84%]">
+        <div>
+          <Header />
+        </div>
+        <div className="p-10 rounded">{children}</div>
+      </div>
+    </div>
+  );
 }
 
 export default Layout;
--- src/pages/homePage/index.jsx
+++ src/pages/homePage/index.jsx
@@ -3,7 +3,12 @@ import SearchBar from "../../components/searchComponent/searchBar";
 
 function HomePage() {
   return (
-    <div className="bg-white text-black h-[vh] text-3xl  border-solid border-2 border-gray-300 rounded"></div>
+    <div className="w-full bg-black h-full p-4">
+      <div className="flex items-center justify-center ">
+        <h2 className="text-[16px] text-[white] p-4">DISTRICT HOME PAGE</h2>
+      </div>
+      <div className="bg-[#2d3c53] text-black rounded">hello</div>
+    </div>
   );
 }
 export default HomePage;

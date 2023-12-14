2ccfd637d5ac62a3056245656e3c7c964e5f18dd
vikasveltetech <vikas.mishra@sveltetech.com>
2023-12-14 14:30:23 +0530

    disable

--- src/components/radio/index.jsx
+++ src/components/radio/index.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import React from "react";
 
-function Radio({label="label",value,name,handleChange}) {
+function Radio({ label = "label", value, name, handleChange, disable }) {
   return (
-    
-<fieldset>
-
-  <div class="flex items-center mb-4">
-    <input id={name} onClick={handleChange} checked={value} type="radio" name={name} class="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600" />
-    <label for="country-option-1" className="block ml-2 text-sm font-medium text-gray-900 dark:text-gray-300" id={name}>
-    {label}
-    </label>
-  </div>
-
-
-</fieldset>
-
-  )
+    <fieldset>
+      <div class="flex items-center mb-4">
+        <input
+          id={name}
+          onClick={handleChange}
+          checked={value}
+          type="radio"
+          name={name}
+          disabled
+          class="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600"
+        />
+        <label
+          for="country-option-1"
+          className="block ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          id={name}
+        >
+          {label}
+        </label>
+      </div>
+    </fieldset>
+  );
 }
 
-export default Radio
+export default Radio;

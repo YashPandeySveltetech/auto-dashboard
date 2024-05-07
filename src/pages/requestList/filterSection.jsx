2d03be6688f4d2eba26bed1c891aebc57f871148
anshulkumar112 <anshul.kumar@sveltetech.com>
2024-05-07 17:41:28 +0530

    filter search

--- src/pages/requestList/filterSection.jsx
+++ src/pages/requestList/filterSection.jsx
@@ -3,17 +3,23 @@
 import React, { useEffect, useState } from "react";
 import CommonDropDown from "../../components/dropdown";
 import Input from "../../components/input";
-import Datepicker from "react-tailwindcss-datepicker"; 
-import {  GET_POLICE_STATION_LIST } from "../../utils/constants";
+import Datepicker from "react-tailwindcss-datepicker";
+import { GET_POLICE_STATION_LIST } from "../../utils/constants";
 import { ApiHandle } from "../../utils/ApiHandle";
 import { useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
 
-
-function FilterSection({ filter, setFilter, getAllRequest,dateRange,setDateRange,exportReport,clearFilter }) {
-	const { rank } = useSelector((state) => state.user?.userData);
-  const location=useLocation()
- 
+function FilterSection({
+  filter,
+  setFilter,
+  getAllRequest,
+  dateRange,
+  setDateRange,
+  exportReport,
+  clearFilter,
+}) {
+  const { rank } = useSelector((state) => state.user?.userData);
+  const location = useLocation();
 
   const from_status_option = [
     { id: 1, name: "PENDING", value: "PENDING" },
@@ -26,47 +32,45 @@ function FilterSection({ filter, setFilter, getAllRequest,dateRange,setDateRange
     { id: 3, name: "TDR", value: "TDR" },
     { id: 4, name: "CAF", value: "CAF" },
   ];
-  const target_type_option=[
+  const target_type_option = [
     { id: 1, name: "MOBILE_NUMBER", value: "MOBILE_NUMBER" },
     { id: 2, name: "IMEI_NUMBER", value: "IMEI_NUMBER" },
     { id: 3, name: "CELL_ID", value: "CELL_ID" },
     { id: 4, name: "IP_ADDRESS", value: "IP_ADDRESS" },
     { id: 5, name: "ILD", value: "ILD" },
-  ]
-  const [policeStation,setPoliceStation]=useState([])
-useEffect(() => {
+  ];
+  const [policeStation, setPoliceStation] = useState([]);
+  useEffect(() => {
     getPoliceStaionList();
-}, []);
-const getPoliceStaionList = async () => {
+  }, []);
+  const getPoliceStaionList = async () => {
     const res = await ApiHandle(`${GET_POLICE_STATION_LIST}`, {}, "GET");
     if (res.statusCode === 200) {
       const data = res?.responsePayload;
-    //   setPoliceStationOptions(data);
-    // setPoliceStation(data)
-  
-   let arr=[]
-    if(data.length){
-        for(let i=0;i<=data.length;i++){
+      //   setPoliceStationOptions(data);
+      // setPoliceStation(data)
 
-arr.push({...data[i],['value']:data[i]?.id})
+      let arr = [];
+      if (data.length) {
+        for (let i = 0; i <= data.length; i++) {
+          arr.push({ ...data[i], ["value"]: data[i]?.id });
         }
-        
-        setPoliceStation(arr)
-    }
-  
+
+        setPoliceStation(arr);
+      }
+
       return;
     }
   };
 
-const handleValueChange = newValue => {
-    
+  const handleValueChange = (newValue) => {
     setDateRange(newValue);
-};
+  };
   return (
-    <div className="flex">
-        <div className="flex flex-col w-[90%]">
-    <div className="flex w-[100%] justify-between p-5">
-      {/* <div>
+    <div className="flex border ml-5">
+      <div className=" flex-flex-row w-[100%]">
+        <div className="flex flex-col w-[100%] justify-between p-2 items-center">
+          {/* <div>
         <CommonDropDown
           name={"req_to_provider"}
           options={req_to_provider_option}
@@ -76,79 +80,121 @@ const handleValueChange = newValue => {
           label="Request to provider"
         />
       </div> */}
-      {["/unverified-form","/rejected-form"].includes(location.pathname)?"": <div>
-        <label htmlFor=""> Form Status</label>
-        <CommonDropDown
-          name={"form_status"}
-          options={from_status_option}
-          onChange={(e) => {
-            setFilter({ ...filter, [e.target.name]: e.target.value });
-          }}
-          label=""
-          value={filter["form_status"]}
-        />
-      </div>}
-     
-      <div className="" >
-      <label htmlFor=""> Select Date</label>
-      <Datepicker 
-primaryColor={"blue"} 
-value={dateRange}
-onChange={handleValueChange} 
-showShortcuts={true} 
-
-/> 
-      </div>
-      <div className="" >
-      <label htmlFor=""> Select Type</label>
-      <CommonDropDown
-          name={"target_type"}
-          options={target_type_option}
-          onChange={(e) => {
-            setFilter({ ...filter, [e.target.name]: e.target.value });
-          }}
-          label=""
-          value={filter["target_type"]}
-        />
-      </div>
-      {filter["target_type"]!=="" &&
-      <div className="" >
-      <label htmlFor=""> Select Target Type Value</label>
-      <Input
-                  type="text"
-                  value={filter["target_type_value"]}
-                  required={true}
-                  name="target_type_value"
-                  onChange={(e) => setFilter({ ...filter, [e.target.name]: e.target.value })}
-                
-                  className="w-[100%]"
+          <div className="w-full flex justify-between gap-4">
+            {["/unverified-form", "/rejected-form"].includes(
+              location.pathname
+            ) ? (
+              ""
+            ) : (
+              <div className="w-full">
+                <label htmlFor=""> Form Status</label>
+                <CommonDropDown
+                  name={"form_status"}
+                  options={from_status_option}
+                  onChange={(e) => {
+                    setFilter({ ...filter, [e.target.name]: e.target.value });
+                  }}
+                  label=""
+                  value={filter["form_status"]}
                 />
-      </div>}
-      <button
-     onClick={getAllRequest}
-     style={{width:"100px", border:"2px solid green",borderRadius:"20px",height:"40px",marginTop:"20px"}}
-     className="m-5 mt-10"
-   >
-     <b>Search</b>
-   </button>
-    {['DCP'].includes(rank)&&  <button
-     onClick={exportReport}
-     style={{width:"100px", border:"2px solid green",borderRadius:"20px",height:"40px",marginTop:"20px"}}
-     className="m-5 mt-10"
-     disabled={dateRange?.startDate==""}
-   >
-     <b>Export File</b>
-   </button>}
-   <button
-     onClick={clearFilter}
-     type="button"
-     style={{width:"100px", border:"2px solid green",borderRadius:"20px",height:"40px",marginTop:"20px"}}
-     className="m-5 mt-10"
+              </div>
+            )}
+            <div className="w-full">
+              <label htmlFor=""> Select Type</label>
+              <CommonDropDown
+                name={"target_type"}
+                options={target_type_option}
+                onChange={(e) => {
+                  setFilter({ ...filter, [e.target.name]: e.target.value });
+                }}
+                label=""
+                value={filter["target_type"]}
+              />
+            </div>
+          </div>
 
-   >
-     <b>Clear Filter</b>
-   </button>
-      {/* <div>
+          
+          <div className="flex w-full justify-between gap-4">
+          <div className="w-full">
+            <label htmlFor=""> Select Date</label>
+           <div className="text-black-900 border border-gray-300 rounded-lg bg-blue-100 focus:ring-blue-500 focus:border-blue-500">
+           <Datepicker
+              primaryColor={"lightgray"}
+              value={dateRange}
+              onChange={handleValueChange}
+              showShortcuts={true}
+              classNames="border border-solid"
+            />
+           </div>
+          </div>
+         
+            <div className="w-full  ">
+            {filter["target_type"] !== "" && (<>
+            
+              <label htmlFor=""> Select Target Type Value</label>
+              <Input
+                type="text"
+                value={filter["target_type_value"]}
+                required={true}
+                name="target_type_value"
+                onChange={(e) =>
+                  setFilter({ ...filter, [e.target.name]: e.target.value })
+                }
+                className="w-[100%]"
+              />
+              </>
+          )}
+            </div>
+          </div>
+          
+     <div>
+     <button
+            onClick={getAllRequest}
+            style={{
+              width: "100px",
+              border: "2px solid green",
+              borderRadius: "20px",
+              height: "40px",
+              marginTop: "20px",
+            }}
+            className="m-5 mt-10"
+          >
+            <b>Search</b>
+          </button>
+          {["DCP"].includes(rank) && (
+            <button
+              onClick={exportReport}
+              style={{
+                width: "100px",
+                border: "2px solid green",
+                borderRadius: "20px",
+                height: "40px",
+                marginTop: "20px",
+              }}
+              className="m-5 mt-10"
+              disabled={dateRange?.startDate === ""}
+            >
+              <b>Export File</b>
+            </button>
+          )}
+            <button
+            onClick={clearFilter}
+            type="button"
+            style={{
+              width: "100px",
+              border: "2px solid green",
+              borderRadius: "20px",
+              height: "40px",
+              marginTop: "20px",
+            }}
+            className="m-5 mt-10"
+          >
+            <b>Clear Filter</b>
+          </button>
+     </div>
+          
+        
+          {/* <div>
         <Input
           name="case_ref"
           onChange={(e) => {
@@ -157,7 +203,7 @@ showShortcuts={true}
           label="FIR NO."
         />
       </div> */}
-      {/* <div>
+          {/* <div>
         <Input
           name="case_type"
           onChange={(e) => {
@@ -166,44 +212,25 @@ showShortcuts={true}
           label="Case Type"
         />
       </div> */}
-     
-    </div>
-    <div className="flex w-[100%] justify-between p-5">
-    
-      <div>
-     {['DCP'].includes(rank)&&   <CommonDropDown
-          name={"police_station"}
-          options={policeStation}
-          checkId={true}
-          onChange={(e) => {
-            setFilter({ ...filter, [e.target.name]: e.target.value });
-          }}
-          label="Police Station"
-        />}
+        </div>
+        <div className="flex w-[100%] justify-between p-5">
+          <div>
+            {["DCP"].includes(rank) && (
+              <CommonDropDown
+                name={"police_station"}
+                options={policeStation}
+                checkId={true}
+                onChange={(e) => {
+                  setFilter({ ...filter, [e.target.name]: e.target.value });
+                }}
+                label="Police Station"
+              />
+            )}
+          </div>
+       
+        </div>
       </div>
-      {/* <div>
-        <Input
-          name="case_ref"
-          onChange={(e) => {
-            setFilter({ ...filter, [e.target.name]: e.target.value });
-          }}
-          label="FIR NO."
-        />
-      </div>
-      <div>
-        <Input
-          name="case_type"
-          onChange={(e) => {
-            setFilter({ ...filter, [e.target.name]: e.target.value });
-          }}
-          label="Case Type"
-        />
-      </div> */}
-     {}
     </div>
-</div>
-    
-   </div>
   );
 }
 
--- src/pages/requestPage/index.jsx
+++ src/pages/requestPage/index.jsx
@@ -829,7 +829,7 @@ setIldList([
           </div>
           <div className="mt-6 flex items-center gap-6">
             <label className="font-bold required">Target Type:</label>
-            <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+            <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200">
               <ul className="flex flex-wrap -mb-px">
                 {targetType?.map((val, key) => (
                   <li className="me-2" key={key}>
